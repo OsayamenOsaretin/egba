20b1e91d8c567a6e2391c3e65d0d664056caaab0
yamenO <yamen@jtpk.co>
2019-10-12 06:49:54 +0100

    integrate expo contacts

--- egba/App.js
+++ egba/App.js
@@ -1,7 +1,20 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as Contacts from "expo-contacts";
+
+const useGetContacts = async (setContacts) => {
+  const { data } = await Contacts.getContactsAsync();
+  if (data.length > 0) {
+    setContacts(data)
+  }
+};
 
 export default function App() {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    useGetContacts(setContacts);
+  })
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -12,8 +25,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
